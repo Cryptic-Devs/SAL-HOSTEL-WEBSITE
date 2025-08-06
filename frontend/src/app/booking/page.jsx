d3b5d5b8d33cacdf681b3b45b4e281128254f2d5
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-06 18:29:02 +0000

    create the booking page

---
+++ frontend/src/app/booking/page.jsx
@@ -0,0 +1,8 @@
+import BookingWrapper from "@/components/BookingWrapper";
+export default function BookingPage() {
+  return(
+    <BookingWrapper subTitle="Booking">
+
+    </BookingWrapper>
+  );
+}
