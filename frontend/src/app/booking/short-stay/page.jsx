a6f574313134d0953b273c1435a37525fdfcbd48
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-06 18:29:39 +0000

    create the short stay booking page

---
+++ frontend/src/app/booking/short-stay/page.jsx
@@ -0,0 +1,8 @@
+import BookingWrapper from "@/components/BookingWrapper";
+export default function ShortStayBooking() {
+  return(
+    <BookingWrapper subTitle="Short Stay Booking">
+
+    </BookingWrapper>
+  );
+}
