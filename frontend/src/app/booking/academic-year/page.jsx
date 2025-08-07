f5fcf127451926552b0b0a9f6819a61ec86c8b4a
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-07 22:40:37 +0000

    create the academic year booking page

--- frontend/src/app/booking/academic-year/page.jsx
+++ frontend/src/app/booking/academic-year/page.jsx
@@ -1,8 +1,11 @@
+import BookingPage from "@/components/BookingPage";
 import BookingWrapper from "@/components/BookingWrapper";
-export default function AcademicYearBooking() {
-  return(
-    <BookingWrapper subTitle="Academic Year Booking">
 
+export default function AcademicYearBooking() {
+
+  return (
+    <BookingWrapper subTitle="Academic Year Booking">
+      <BookingPage />
     </BookingWrapper>
   );
 }
