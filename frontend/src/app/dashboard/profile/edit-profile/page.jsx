a9acc9c9c828a1fb66c8cfc36876cadb24d3032a
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-09 22:42:34 +0000

    create edit password and profile pages

---
+++ frontend/src/app/dashboard/profile/edit-profile/page.jsx
@@ -0,0 +1,9 @@
+import DashboardWrapper from "@/components/DashboardWrapper";
+
+export default function EditProfile() {
+  return (
+    <DashboardWrapper>
+      <div>Edit Profile Page</div>
+    </DashboardWrapper>
+  );
+}
