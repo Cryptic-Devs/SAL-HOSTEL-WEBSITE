bca6ce9624400eaab8ae43c26d04200a3ab00edf
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-10 18:33:22 +0000

    Eastwood sign up page (#26)
    * my work isnot done yet
    
    * create the booking page
    
    * create the academic year booking page
    
    * create the short stay booking page
    
    * sign in page
    
    * sign in page
    
    * create booking page
    
    * create the academic year booking page
    
    * create a booking page component
    
    * create short stay booking page
    
    * build the payment page
    
    * create the payment confirmation page
    
    * change input arrangement in the booking page component
    
    * make the booking wrapper responsive
    
    * fix sign in button on booking page
    
    * create a responsive student dashboard wrapper
    
    * create the dashboard pages
    
    * create edit password and profile pages
    
    * Add dashboard and profile page
    
    * fix routing error
    
    * build available beds page
    
    * buld policies page
    
    * build report fault page
    
    ---------
    
    Co-authored-by: Fauziya Adjeley <adjeifauziyaadjeley@gmail.com>

---
+++ frontend/src/app/booking/academic-year/page.jsx
@@ -0,0 +1,11 @@
+import BookingPage from "@/components/BookingPage";
+import BookingWrapper from "@/components/BookingWrapper";
+
+export default function AcademicYearBooking() {
+
+  return (
+    <BookingWrapper subTitle="Academic Year Booking">
+      <BookingPage />
+    </BookingWrapper>
+  );
+}
