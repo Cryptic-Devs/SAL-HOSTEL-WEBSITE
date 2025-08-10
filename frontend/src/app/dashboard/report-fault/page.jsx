44a97f150f066bb06c45bedbffb097c28fd07e5c
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-10 18:29:57 +0000

    build report fault page

--- frontend/src/app/dashboard/report-fault/page.jsx
+++ frontend/src/app/dashboard/report-fault/page.jsx
@@ -1,9 +1,22 @@
 import DashboardWrapper from "@/components/DashboardWrapper";
+import Link from "next/link";
 
 export default function ReportFault() {
   return (
     <DashboardWrapper>
-      <div>Report Fault Page</div>
+      <div className="h-screen flex flex-col mt-5 md:mt-20 items-center gap-20">
+
+        <div className="w-full max-w-lg">
+          <label className="block text-sm font-medium mb-1">Enter Fault Details</label>
+          <input type="text" className="border border-gray-300 w-full p-2 rounded-lg focus:border-blue-600 focus:outline-none max-w-xsm" placeholder="Enter fault Details" />
+        </div>
+
+        <button className="rounded bg-red-500 cursor-pointer hover:bg-red-400 active:bg-red-500 active:scale-x-95 text-white p-3 w-full max-w-lg">
+          Send Report
+        </button>
+        
+      </div>
+
     </DashboardWrapper>
   );
 }
