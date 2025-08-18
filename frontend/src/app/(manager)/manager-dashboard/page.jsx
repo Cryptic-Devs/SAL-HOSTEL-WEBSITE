54a11b36771589fc91e4f4889e348f219e16e714
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2025-08-18 14:53:39 +0000

    create a navbar componet for the admin pages

---
+++ frontend/src/app/(manager)/manager-dashboard/page.jsx
@@ -0,0 +1,9 @@
+import ManagerWrapper from "@/components/ManagerWrapper";
+
+export default function ManagerDashboard() {
+  return (
+    <ManagerWrapper>
+      <h1>Dashboard</h1>
+    </ManagerWrapper>
+  );
+}
