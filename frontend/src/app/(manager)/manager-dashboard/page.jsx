e74b418720d6552dd52169976f0e5ede84f93f60
Fauziya Adjeley <adjeifauziyaadjeley@gmail.com>
2025-08-21 10:31:44 +0100

    deleye files

--- frontend/src/app/(manager)/manager-dashboard/page.jsx
+++
@@ -1,9 +0,0 @@
-import ManagerWrapper from "@/components/ManagerWrapper";
-
-export default function ManagerDashboard() {
-  return (
-    <ManagerWrapper>
-      <h1>Dashboard</h1>
-    </ManagerWrapper>
-  );
-}
